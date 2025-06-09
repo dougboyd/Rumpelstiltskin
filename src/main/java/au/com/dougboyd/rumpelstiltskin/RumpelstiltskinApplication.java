a70c70281fdb3b54c61a13d8ff65276d5ca76cd4
Doug Boyd <doug@dougboyd.com.au>
2025-06-09 22:19:28 +1000

    Add pom.xml, main application class and properties

---
+++ src/main/java/au/com/dougboyd/rumpelstiltskin/RumpelstiltskinApplication.java
@@ -0,0 +1,11 @@
+package au.com.dougboyd.rumpelstiltskin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumpelstiltskinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RumpelstiltskinApplication.class, args);
+    }
+}
